4d37c089d9270d4314895378ff84bd109c542a40
Arraht <igor_161995@mail.ru>
2024-03-01 15:49:00 +0600

    homework solution

--- src/main/java/com/calculator/Homework16/Main.java
+++ src/main/java/com/calculator/Homework16/Main.java
@@ -4,10 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Homework16Application {
+public class Main {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Homework16Application.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
 }
